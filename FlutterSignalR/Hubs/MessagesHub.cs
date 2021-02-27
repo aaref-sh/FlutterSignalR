d39b34e6343f4f4bf41f489330e37ff8dc492cd0
aaref-sh <m.aaref-sh@hotmail.com>
2021-02-27 09:17:35 +0200

    sendmessage function added

--- FlutterSignalR/Hubs/MessagesHub.cs
+++ FlutterSignalR/Hubs/MessagesHub.cs
@@ -18,9 +18,19 @@ namespace FlutterSignalR.Hubs
         }
         public override async Task OnConnectedAsync()
         {
-            Console.WriteLine(Context.ConnectionId);
+            Console.WriteLine(Context.ConnectionId + " connected");
             await Clients.All.SendAsync("getid");
             await base.OnConnectedAsync();
+        }
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            Console.WriteLine(Context.ConnectionId + " disconnected");
+            foreach (var id in map)
+            {
+                if (id.Value == Context.ConnectionId) map.Remove(id.Key);
+            }
+
+            await base.OnDisconnectedAsync(exception);
         }
         public async Task getid(int x)
         {
@@ -28,6 +38,27 @@ namespace FlutterSignalR.Hubs
             var messagelist = (from m in db.Message where m.Reciever == x select m).ToList();
             await Clients.Client(map[x]).SendAsync("idok", x);
             await Clients.Client(map[x]).SendAsync("newmessages", messagelist);
+        }
+        public async Task SendMessage(int sender,int receiver,string message)
+        {
+            Message m = new Message();
+            m.Sender = sender;
+            m.Reciever = receiver;
+            m.Msg = message;
+            m.Date = DateTime.Now;
+            if (map[receiver] != null)
+            {
+
+                db.Message.Add(m);
+                db.SaveChanges();
+            }
+            else
+            {
+                List<Message> messagelist = new List<Message>();
+                messagelist.Add(m);
+                await Clients.Client(map[receiver]).SendAsync("newmessages", messagelist);
+
+            }
         }
     }
 }

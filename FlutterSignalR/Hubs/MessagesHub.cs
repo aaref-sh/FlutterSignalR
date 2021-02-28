19d509793e236fd25b3dfc659db74fa4f024644c
aaref-sh <m.aaref-sh@hotmail.com>
2021-02-28 18:10:15 +0200

    fix confid and ondisconnect

--- FlutterSignalR/Hubs/MessagesHub.cs
+++ FlutterSignalR/Hubs/MessagesHub.cs
@@ -9,7 +9,8 @@ namespace FlutterSignalR.Hubs
 {
     public class MessagesHub : Hub
     {
-        Dictionary<int, string> map = new Dictionary<int, string>();
+        private static readonly Dictionary<int, string> map = new Dictionary<int, string>();
+        private static readonly Dictionary<string, int> mp1 = new Dictionary<string, int>();
         lanchatContext db = new lanchatContext();
         public async Task moveviewfromserver()
         {
@@ -25,20 +26,17 @@ namespace FlutterSignalR.Hubs
         public override async Task OnDisconnectedAsync(Exception exception)
         {
             Console.WriteLine(Context.ConnectionId + " disconnected");
-            foreach (var id in map)
-                if (id.Value == Context.ConnectionId){
-                    map.Remove(id.Key);
-                    break;
-            }
+            map.Remove(mp1[Context.ConnectionId]);
+            mp1.Remove(Context.ConnectionId);
             await base.OnDisconnectedAsync(exception);
         }
         public async Task confid(int id)
         {
-            map.Add(id,Context.ConnectionId);
+            map.Add(id, Context.ConnectionId);
+            mp1.Add(Context.ConnectionId, id);
             var messagelist = (from m in db.Messages where m.Receiver == id select m).ToList();
-            await Clients.Client(map[id]).SendAsync("idok", id);
-            await Clients.Client(map[id]).SendAsync("newmessages", messagelist);
-            //foreach (var x in messagelist) db.Messages.Remove(x);
+            await Clients.Client(Context.ConnectionId).SendAsync("newmessages", messagelist);
+            foreach (var x in messagelist) db.Messages.Remove(x);
             db.SaveChanges();
         }
         public async Task sendmessage(int sender,int receiver,string message)
@@ -51,17 +49,12 @@ namespace FlutterSignalR.Hubs
             if (map[receiver] == null) { db.Messages.Add(m); db.SaveChanges(); }
             else
             {
-                List<Message> messagelist = new List<Message>();
-                messagelist.Add(m);
+                List<Message> messagelist = new List<Message>{m};
                 await Clients.Client(map[receiver]).SendAsync("newmessages", messagelist);
             }
             
         }
-        public async Task getidname(int id)
-        {
-            var name = (from x in db.Users where x.Id == id select x.Name).First();
-            await Clients.Client(Context.ConnectionId).SendAsync("setidname", id, name);
-        }
+        public string getidname(int id) => (from x in db.Users where x.Id == id select x.Name).First();
         public int signup(string name,string pass)
         {
 
